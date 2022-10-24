40f2e4c2d212c0ed2c68f53e182b705c05eea951
Terry Miller <millerterryt@gmail.com>
2022-10-24 14:45:08 -0400

    Added add_new_user with opening of json file read and dump

--- project.py
+++ project.py
@@ -2,20 +2,28 @@
 import pyinputplus
 
 
-def req_init_menu_response():
+def req_init_menu_selection():
         """prints initial menu. requests int input. returns int"""
-        print("Select an option:")
-        print("1. Select from existing user list")
+        print("1. Select existing user")
         print("2. create new user")
+        print("Enter a number:")
         selection = pyinputplus.inputNum(min=1, max=2)
         return int(selection)
 
-def create_new_user():
-        pass
+def req_new_user():
+        print("Enter a username:")
+        user_name = input()
+        return user_name
 
-def main():
-        init_menu_selection = req_init_menu_response()
-        pass
+def add_new_user(user_name):
+        with open("todo_user_database.json", "r") as jsonFile:
+                data = json.load(jsonFile)
+        data[user_name] = []
+        with open("todo_user_database.json", "w") as jsonFile:
+                json.dump(data, jsonFile)
 
-if __name__ == '__main__':
-	main()
+init_menu_selection = req_init_menu_selection()
+if init_menu_selection == 1:
+        user_name = req_new_user()
+
+
