aab5c35a31e9fd43edb910c745c1884a6342c346
Terry Miller <millerterryt@gmail.com>
2022-10-24 13:38:38 -0400

    Added menu 1 and options. Added user input for response.

--- project.py
+++ project.py
@@ -1,7 +1,15 @@
 ##
 
+def req_menu1_response():
+        print("Select an option:")
+        print("1. Select from existing user list")
+        print("2. create new user")
+
+        
+        response = input("> ")
+
 def main():
-	pass
+        pass
 
 if __name__ == '__main__':
 	main()
